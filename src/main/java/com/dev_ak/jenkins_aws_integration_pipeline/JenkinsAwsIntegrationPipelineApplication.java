6c8f40bf9290919a49aaf92b0c4f3c8e8f2f6eda
Atul Kumar <atulkumar@Atuls-MacBook-Pro.local>
2022-11-02 15:25:39 -0400

    first commit

---
+++ src/main/java/com/dev_ak/jenkins_aws_integration_pipeline/JenkinsAwsIntegrationPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.dev_ak.jenkins_aws_integration_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsAwsIntegrationPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsAwsIntegrationPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev_ak/jenkins_aws_integration_pipeline/JenkinsAwsIntegrationPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_ak.jenkins_aws_integration_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAwsIntegrationPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
